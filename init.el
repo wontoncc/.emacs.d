2bf26f24b21989bbecb488d2b9c625197a963ebe
Jason Chen <wonton.cc@gmail.com>
2016-10-15 17:12:59 +0800

    Add `custom.el` to contain all the generated code for the customization.

--- init.el
+++ init.el
@@ -1,10 +1,14 @@
 (setq inhibit-startup-screen t)
 
+(prefer-coding-system 'utf-8)
+
 (setq backup-by-copying t)
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -14,17 +18,3 @@
 (load "~/.emacs.d/interface.el")
 (load "~/.emacs.d/languages.el")
 (load "~/.emacs.d/kbds.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (which-key web-mode use-package telephone-line spaceline rainbow-delimiters projectile powerline-evil nlinum markdown-mode leuven-theme js2-mode hydandata-light-theme evil-nerd-commenter dired-rainbow counsel company-tern browse-kill-ring async ample-theme aggressive-indent ace-window ac-cider))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
